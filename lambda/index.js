9515c495f887f228579cead43af7e8cb21ee7b58
Zach Cannon <Zach.Cannon@gmail.com>
2017-06-15 14:04:44 -0500

    updated template to make URI dynamic

--- lambda/index.js
+++ lambda/index.js
@@ -1,12 +1,7 @@
 'use strict';
-
-
 const AWS = require('aws-sdk');
 
-const tableName = process.env.TABLE_NAME;
-
 const createResponse = (statusCode, body) => {
-
     return {
         statusCode: statusCode,
         body: body
@@ -14,7 +9,6 @@ const createResponse = (statusCode, body) => {
 };
 
 exports.put = (event, context, callback) => {
-
     console.log(`PUT ITEM SUCCEEDED WITH doc = ${JSON.stringify(event)}`);
-    callback(null, createResponse(200, null));
+    callback(null, createResponse(200, {"I got":"Into Lambda"}));
 };
