fc64e3fb0b524a820955e4ac8abdee12be8aa3d3
Michael Doberenz <fuwjax@gmail.com>
2017-06-16 09:57:05 -0500

    Refactor into src/target

--- uploadToCloudFormation.sh
+++ uploadToCloudFormation.sh
@@ -2,13 +2,16 @@
 
 BUCKET=$1
 
+rm -rf ./target
+mkdir target
+
 aws cloudformation package \
-    --template-file ./sam/template.yaml \
+    --template-file ./src/template.yaml \
     --s3-bucket $BUCKET \
-    --output-template-file ./sam/packaged-template.yaml
-    
+    --output-template-file ./target/packaged-template.yaml
+
 aws cloudformation deploy \
-    --template-file ./sam/packaged-template.yaml \
+    --template-file ./target/packaged-template.yaml \
     --stack-name Sonic-DOM-Passthrough \
     --capabilities CAPABILITY_IAM \
     --parameter-overrides S3Bucket=$BUCKET
